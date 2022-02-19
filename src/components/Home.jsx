2e71ebca30dad58a0c080522d453f0aec5f302ab
Your Name <diegorigo2@gmail.com>
2022-02-19 14:46:02 -0300

    subindo a logica do site

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import MenuNav from './MenuNav';
+import Cart from './Cart';
+import Produtos from './Produtos';
+import Category from './Category';
+import Footer from './Footer';
+import { PagProdutos } from './PagProdutos';
+
+
+export default class Home extends React.Component {
+    render() {
+      return (
+        <div className="Home">
+            <MenuNav />
+            < PagProdutos />
+           
+        </div>
+      
+      );
+    }
+  }
+
+
